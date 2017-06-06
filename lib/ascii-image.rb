eac51cf6f9eef246529d00abfb2a1718a7d5153d
bprasetyo <budi.prasetyo@turboly.com>
2017-06-06 10:29:22 +0700

    Update ascii-image.rb
    update

--- lib/ascii-image.rb
+++ lib/ascii-image.rb
@@ -32,6 +32,10 @@ class ASCII_Image
     def initialize(uri, console_width = 80)
         @uri = uri
         @console_width = console_width
+        
+        if Magick::MAGICKCORE_QUANTUM_DEPTH > 8
+            raise "Your ImageMagick quantum depth is set to #{Magick::MAGICKCORE_QUANTUM_DEPTH}. You need to have it set to 8 in order for this app to work."
+        end
     end
     
     # Convert the image into ASCII and print it to the console.
